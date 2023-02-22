c93f49a35475f1ffdaa93c8449f44c65920add7e
Dorcy Jean Duke <dorcyd89@gmail.com>
2023-02-22 09:54:28 -0500

    feat: create CatCard component

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const cat: ICatCard = {
+  tag: 'Felines',
+  title: "What's new in Cats",
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing el aspect, sed do eiusmod tempor incididunt ut labore',
+  author: 'Alex',
+  time: '2h ago',
+};
+
+export const mockCatCardProp = {
+  cat,
+};
